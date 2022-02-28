db99bfdbcdb043b1bef61fcd5d31ac58a0ac4940
Pheon-Dev <devpheon@gmail.com>
2022-02-28 06:18:32 +0300

    Overflow x axis

--- vision-lms-frontend/src/components/Components/Layout.jsx
+++ vision-lms-frontend/src/components/Components/Layout.jsx
@@ -9,7 +9,7 @@ const Layout = ({ members }) => {
         All Registered Members
       </div>
       <br />
-      <div className="-my-2 sm:-mx-6 lg:-mx-8">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="py-2 align-middle inline-block min-w-full sm:px-8">
           <div className="shadow overflow-hidden mr-3 ml-3 border-b border-gray-200 sm:rounded-lg">
             <table className="min-w-full divide-y divide-gray-200">
--- vision-lms-frontend/src/components/Loan/LoansFeed.jsx
+++ vision-lms-frontend/src/components/Loan/LoansFeed.jsx
@@ -96,7 +96,7 @@ export default function LoansFeed() {
           <span className="text-gray-500 ml-auto mr-auto">(pending approval)</span>
         </div>
         <br />
-        <div className="-my-2 sm:-mx-6 lg:-mx-8">
+        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="py-2 align-middle inline-block min-w-full sm:px-8">
             <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
               <table className="min-w-full divide-y divide-gray-200">
@@ -159,7 +159,7 @@ export default function LoansFeed() {
           <span className="text-gray-500 ml-auto mr-auto">(pending disbursal)</span>
         </div>
         <br />
-        <div className="-my-2 sm:-mx-6 lg:-mx-8">
+        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="py-2 align-middle inline-block min-w-full sm:px-8">
             <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
               <table className="min-w-full divide-y divide-gray-200">
@@ -222,7 +222,7 @@ export default function LoansFeed() {
           <span className="text-gray-500 ml-auto mr-auto">(pending disbursal)</span>
         </div>
         <br />
-        <div className="-my-2 sm:-mx-6 lg:-mx-8">
+        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="py-2 align-middle inline-block min-w-full sm:px-8">
             <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
               <table className="min-w-full divide-y divide-gray-200">
@@ -283,7 +283,7 @@ export default function LoansFeed() {
           <span className="text-gray-500 ml-auto mr-auto">(All disbursed loans)</span>
         </div>
         <br />
-        <div className="-my-2 sm:-mx-6 lg:-mx-8">
+        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="py-2 align-middle inline-block min-w-full sm:px-8">
             <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
               <table className="min-w-full divide-y divide-gray-200">
