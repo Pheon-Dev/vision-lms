5de0dce9d83c2b359f250b446cd017724e768593
Pheon-Dev <devpheon@gmail.com>
2022-07-29 09:40:08 +0300

    ADD: Post Transactions to DB && Fetch to Display

--- vision-lms-frontend/src/components/Admin/pages/ExpensesAppModal.jsx
+++ vision-lms-frontend/src/components/Admin/pages/ExpensesAppModal.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ModalAlert } from "../../Modals";
+import { ListLayout, Spinner, Status, TableData } from "../../Components";
+import { BsCheck2Circle } from "react-icons/bs";
+import { client } from "../../../client";
 
 const ExpensesAppModal = ({
   closeApp,
@@ -12,11 +15,31 @@ const ExpensesAppModal = ({
   const [transactions, setTransactions] = useState(false);
   const [budget, setBudget] = useState(false);
   const [miscellaneous, setMiscellaneous] = useState(false);
+  const [openSubmit, setOpenSubmit] = useState(false);
+  const [processing, setProcessing] = useState(false);
   const [title, setTitle] = useState("Expenses App");
-  const [date, setDate] = useState();
-  const [transactionType, setTransactionType] = useState();
-  const [transactionAmount, setTransactionAmount] = useState();
+  const [transactionDate, setTransactionDate] = useState("");
+  const [transactionType, setTransactionType] = useState("");
+  const [transactionAmount, setTransactionAmount] = useState("");
+  const [recentTransactions, setRecentTransactions] = useState("");
   const [validator, setValidator] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(8);
+
+  const fetchLoanDetails = () => {
+    const query = `*[_type == "transactions"]`;
+    let subscription = true;
+    if (subscription) {
+      client.fetch(query).then((data) => {
+        setRecentTransactions(data);
+      });
+    }
+    return () => (subscription = false);
+  };
+
+  useEffect(() => {
+    fetchLoanDetails();
+  }, []);
 
   const Label = ({ label }) => (
     <>
@@ -30,6 +53,73 @@ const ExpensesAppModal = ({
       </label>
     </>
   );
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = recentTransactions?.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Change page
+  const paginateFront = () => setCurrentPage(currentPage + 1);
+  const paginateBack = () => setCurrentPage(currentPage - 1);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const headers = ["Date", "Type", "Amount"];
+
+  function renderTransactionsTable() {
+    return (
+      <ListLayout
+        title="Recent Transactions List"
+        headers={headers}
+        itemsPerPage={itemsPerPage}
+        totalItems={recentTransactions?.length}
+        paginate={paginate}
+        currentPage={currentPage}
+        paginateBack={paginateBack}
+        paginateFront={paginateFront}
+      >
+        <tbody className="bg-white divide-y divide-gray-200">
+          {currentItems?.map((transaction) =>
+              <tr
+                onClick={() => {
+                     // navigate("/loan/disbursements");
+                  console.log(transaction.transactionAmount)
+                }}
+                key={transaction._id}
+                    className={
+                      transaction.transactionType === "EXPENDITURE"
+                        ? "hover:bg-indigo-200 -py-2 bg-indigo-50 cursor-pointer transition-all duration-300"
+                        : "hover:bg-green-200 bg-green-50 cursor-pointer transition-all duration-300"
+                    }
+              >
+                <TableData>
+                  <div className="ml-0">
+                    <div className="text-sm font-semibold text-gray-900">
+                      {transaction.transactionDate}
+                    </div>
+                  </div>
+                </TableData>
+                <TableData>
+                  <div className="ml-0">
+                    <div className="text-sm font-semibold text-gray-900">
+                      {transaction.transactionType}
+                    </div>
+                  </div>
+                </TableData>
+                <TableData>
+                  <div className="ml-0">
+                    <div className="text-sm font-semibold text-gray-900">
+                      {transaction.transactionAmount}
+                    </div>
+                  </div>
+                </TableData>
+              </tr>
+          )}
+        </tbody>
+      </ListLayout>
+
+    )
+  }
 
   const transactionTypeSelector = [
     {
@@ -46,6 +136,24 @@ const ExpensesAppModal = ({
     },
   ];
 
+  const handleSubmit = () => {
+    setProcessing(true);
+    if (transactionDate && transactionType && transactionAmount) {
+      const doc = {
+        _type: "transactions",
+        transactionDate,
+        transactionType,
+        transactionAmount,
+      };
+      client.create(doc).then(() => {
+        setOpenSubmit(true);
+        setProcessing(false);
+      });
+    } else {
+      setValidator(true);
+    }
+  };
+
   let classInput =
     "rounded-lg bg-gray-700 focus:bg-gray-600 focus:text-gray-200 text-gray-300 focus:outline-none";
   function renderTransactions() {
@@ -60,23 +168,23 @@ const ExpensesAppModal = ({
                   label="Transaction Type"
                   item={transactionType}
                 />
-            <select
-              onChange={(e) => {
-                setTransactionType(e.target.value.toUpperCase());
-                e.target.value.toUpperCase();
-              }}
-              className={classInput}
-            >
-              {transactionTypeSelector.map((item) => (
-                <option
-                  key={item.name}
+                <select
+                  onChange={(e) => {
+                    setTransactionType(e.target.value.toUpperCase());
+                    e.target.value.toUpperCase();
+                  }}
                   className={classInput}
-                  value={item.name}
                 >
-                  {item.name}
-                </option>
-              ))}
-            </select>
+                  {transactionTypeSelector.map((item) => (
+                    <option
+                      key={item.name}
+                      className={classInput}
+                      value={item.name}
+                    >
+                      {item.name}
+                    </option>
+                  ))}
+                </select>
               </div>
               <div className="w-full md:w-1/3">
                 <Label
@@ -93,11 +201,17 @@ const ExpensesAppModal = ({
                 />
               </div>
               <div className="w-full md:w-1/3">
-                <Label valid={validator} label="Date" item={date} />
+                <Label
+                  valid={validator}
+                  label="Transaction Date"
+                  item={transactionDate}
+                />
                 <input
                   type="date"
-                  value={date}
-                  onChange={(e) => setDate(e.target.value.toUpperCase())}
+                  value={transactionDate}
+                  onChange={(e) =>
+                    setTransactionDate(e.target.value.toUpperCase())
+                  }
                   className={classInput}
                 />
               </div>
@@ -107,20 +221,19 @@ const ExpensesAppModal = ({
                 <button
                   data-modal-toggle="defaultModal"
                   onClick={() => {
-                    closeApp;
-                    openExpe;
-                    setTitle("Muiscellaneous Mini App");
-                    setTransactions(false);
-                    setBudget(false);
-                    setMiscellaneous(true);
+                    handleSubmit();
+                    setValidator(true);
                   }}
                   type="button"
                   className="hover:text-white text-black font-bold bg-green-300 m-2 hover:bg-green-600 pl-3 pr-3 p-2 rounded-lg transition-all ease-in-out duration-500"
                 >
-                  Add Transaction
+                  {processing ? "Processing ..." : "Add Transaction"}
                 </button>
               </div>
             </div>
+          {
+            renderTransactionsTable()
+          }
           </div>
         )}
       </>
@@ -152,77 +265,105 @@ const ExpensesAppModal = ({
   }
 
   return (
-    <ModalAlert
-      open={open}
-      onCloseAll={closeAll}
-      onClose={
-        !transactions && !budget && !miscellaneous
-          ? close
-          : () => {
-            setTransactions(false);
-            setBudget(false);
-            setMiscellaneous(false);
-            setTitle("Expenses App");
-          }
-      }
-      title={title}
-      type="expenses"
-    >
-      <div className="flex-auto m-3 justify-center items-center gap-6 md:w-1/2 lg:w-full w-full">
-        {!transactions && !budget && !miscellaneous && (
-          <>
-            <button
-              data-modal-toggle="defaultModal"
-              onClick={() => {
-                closeApp;
-                closeExpe;
-                setTitle("Transactions Mini App");
+    <>
+      <ModalAlert
+        open={open}
+        onCloseAll={closeAll}
+        onClose={
+          !transactions && !budget && !miscellaneous
+            ? close
+            : () => {
+                setTransactions(false);
+                setBudget(false);
+                setMiscellaneous(false);
+                setTitle("Expenses App");
+              }
+        }
+        title={title}
+        type="expenses"
+      >
+        <div className="flex-auto m-3 justify-center items-center gap-6 md:w-1/2 lg:w-full w-full">
+          {!transactions && !budget && !miscellaneous && (
+            <>
+              <button
+                data-modal-toggle="defaultModal"
+                onClick={() => {
+                  closeApp;
+                  closeExpe;
+                  setTitle("Transactions Mini App");
+                  setTransactions(true);
+                  setBudget(false);
+                  setMiscellaneous(false);
+    fetchLoanDetails();
+                }}
+                type="button"
+                className="hover:text-white text-black font-bold bg-blue-300 m-2 hover:bg-blue-600 p-3 rounded-lg transition-all ease-in-out duration-500"
+              >
+                Transactions
+              </button>
+              <button
+                data-modal-toggle="defaultModal"
+                onClick={() => {
+                  closeApp;
+                  openExpe;
+                  setTitle("Budget Mini App");
+                  setTransactions(false);
+                  setBudget(true);
+                  setMiscellaneous(false);
+                }}
+                type="button"
+                className="hover:text-white text-black bg-violet-400 font-bold m-2 hover:bg-violet-600 p-3 rounded-lg transition-all ease-in-out duration-500"
+              >
+                Budget
+              </button>
+              <button
+                data-modal-toggle="defaultModal"
+                onClick={() => {
+                  closeApp;
+                  openExpe;
+                  setTitle("Muiscellaneous Mini App");
+                  setTransactions(false);
+                  setBudget(false);
+                  setMiscellaneous(true);
+                }}
+                type="button"
+                className="hover:text-white text-black bg-green-300 m-2 font-bold hover:bg-green-600 p-3 rounded-lg transition-all ease-in-out duration-500"
+              >
+                Miscellaneous
+              </button>
+            </>
+          )}
+        </div>
+        {renderTransactions()}
+        {renderBudget()}
+        {renderMiscellaneous()}
+      </ModalAlert>
+      <ModalAlert
+        open={openSubmit}
+        onCloseAll={closeAll}
+        onClose={
+          !transactions && !budget && !miscellaneous
+            ? close
+            : () => {
                 setTransactions(true);
                 setBudget(false);
                 setMiscellaneous(false);
-              }}
-              type="button"
-              className="hover:text-white text-black font-bold bg-blue-300 m-2 hover:bg-blue-600 p-3 rounded-lg transition-all ease-in-out duration-500"
-            >
-              Transactions
-            </button>
-            <button
-              data-modal-toggle="defaultModal"
-              onClick={() => {
-                closeApp;
-                openExpe;
-                setTitle("Budget Mini App");
-                setTransactions(false);
-                setBudget(true);
-                setMiscellaneous(false);
-              }}
-              type="button"
-              className="hover:text-white text-black bg-violet-400 font-bold m-2 hover:bg-violet-600 p-3 rounded-lg transition-all ease-in-out duration-500"
-            >
-              Budget
-            </button>
-            <button
-              data-modal-toggle="defaultModal"
-              onClick={() => {
-                closeApp;
-                openExpe;
-                setTitle("Muiscellaneous Mini App");
-                setTransactions(false);
-                setBudget(false);
-                setMiscellaneous(true);
-              }}
-              type="button"
-              className="hover:text-white text-black bg-green-300 m-2 font-bold hover:bg-green-600 p-3 rounded-lg transition-all ease-in-out duration-500"
-            >
-              Miscellaneous
-            </button>
-          </>
-        )}
-      </div>
-      {renderTransactions()}
-      {renderBudget()}
-      {renderMiscellaneous()}
-    </ModalAlert>
+                setOpenSubmit(false);
+                setTitle("Transactions Mini App");
+              }
+        }
+        title="Transaction State"
+        message="Recent Transactions"
+        type="expenses"
+      >
+        <div className="flex items-center w-full">
+          <div className="bg-green-300 opacity-80 relative rounded-full p-2">
+            <BsCheck2Circle className="w-10 font-bold text-black h-10" />
+          </div>
+          <div className="text-md p-3">Transaction Submitted Successfully!</div>
+        </div>
+      </ModalAlert>
+    </>
   );
 };
 
