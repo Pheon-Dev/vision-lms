73512ef022bdec1431af66c58518d5ec629fd27a
Pheon-Dev <devpheon@gmail.com>
2022-08-02 17:08:04 +0300

    DEP: Expenses Bar Chart

--- vision-lms-frontend/src/components/Apps/Expenses.jsx
+++ vision-lms-frontend/src/components/Apps/Expenses.jsx
@@ -1,10 +1,21 @@
 import React, { useState, useEffect } from "react";
+import { FiMinusSquare, FiPlusSquare } from "react-icons/fi";
 import { ListLayout, Spinner, Status } from "../Components";
 import { client } from "../../client";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  CartesianGrid,
+} from "recharts";
 
 const Expenses = () => {
   const [recentExpenditures, setRecentExpenditures] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
+  let [count, setCount] = useState(1000);
   const [itemsPerPage] = useState(8);
 
   const fetchExpenditureDetails = () => {
@@ -29,6 +40,50 @@ const Expenses = () => {
     indexOfLastItem
   );
 
+  const renderBarChart = (
+    <div>
+      <div className="flex justify-end p-2 gap-3">
+        <FiPlusSquare
+          className="cursor-pointer"
+          size={32}
+          color="black"
+          onClick={() => setCount((count += 1000))}
+        />
+        <FiMinusSquare
+          className="cursor-pointer"
+          size={32}
+          color="black"
+          onClick={() =>
+            count < 1000 ? setCount((count -= 100)) : setCount((count -= 1000))
+          }
+        />
+      </div>
+      <BarChart width={1000} height={500} data={recentExpenditures}>
+        <XAxis dataKey="expenditureDate" stroke="#8884d8" />
+        <YAxis
+          // dataKey="expenditureAmount"
+          // stroke="gray"
+          // type="number"
+          domain={[0, count < 0 ? 100 : count]}
+        />
+        <Tooltip wrapperStyle={{ width: 100, backgroundColor: "#ccc" }} />
+        <Legend
+          width={100}
+          wrapperStyle={{
+            top: 40,
+            right: 20,
+            backgroundColor: "#f5f5f5",
+            border: "1px solid #d5d5d5",
+            borderRadius: 3,
+            lineHeight: "40px",
+          }}
+        />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <Bar dataKey="expenditureAmount" fill="#8884d8" barSize={60} />
+      </BarChart>
+    </div>
+  );
+  console.log(count);
   const TableData = ({ children }) => (
     <td className="px-6 py-2 whitespace-nowrap">
       <div className="text-sm font-semibold text-gray-900">{children}</div>
@@ -47,55 +102,56 @@ const Expenses = () => {
         paginateFront={paginateFront}
       >
         <tbody className="bg-white divide-y divide-gray-200">
-          {currentItems.length > 0 && currentItems?.map((expenditure) => (
-            <tr
-              onClick={() => {
-                // navigate("/loan/disbursements");
-                console.log(expenditure.expenditureAmount);
-              }}
-              key={expenditure._id}
-              className={
-                expenditure.expenditureType === "EXPENDITURE"
-                  ? "hover:bg-green-200 -py-2 bg-green-50 cursor-pointer transition-all duration-300"
-                  : "hover:bg-blue-200 bg-blue-50 cursor-pointer transition-all duration-300"
-              }
-            >
-              <TableData>
-                <div className="ml-0">
-                  <div className="text-sm text-gray-900">
-                    {expenditure.expenditureDate}
-                  </div>
-                </div>
-              </TableData>
-              <TableData>
-                <div className="ml-0">
-                  <div className="text-sm text-gray-900">
-                    {expenditure.expenditureDescription}
-                  </div>
-                </div>
-              </TableData>
-              <TableData>
-                <div className="ml-0">
-                  <div className="text-sm text-gray-900">
-                    {expenditure.expenditureType}
-                  </div>
-                </div>
-              </TableData>
-              <TableData>
-                <div className="ml-0">
-                  {expenditure.expenditureType === "EXPENDITURE" ? (
-                    <div className="text-sm font-semibold text-blue-900">
-                      {expenditure.expenditureAmount}
+          {currentItems.length > 0 &&
+            currentItems?.map((expenditure) => (
+              <tr
+                onClick={() => {
+                  // navigate("/loan/disbursements");
+                  console.log(expenditure.expenditureAmount);
+                }}
+                key={expenditure._id}
+                className={
+                  expenditure.expenditureType === "EXPENDITURE"
+                    ? "hover:bg-green-200 -py-2 bg-green-50 cursor-pointer transition-all duration-300"
+                    : "hover:bg-blue-200 bg-blue-50 cursor-pointer transition-all duration-300"
+                }
+              >
+                <TableData>
+                  <div className="ml-0">
+                    <div className="text-sm text-gray-900">
+                      {expenditure.expenditureDate}
                     </div>
-                  ) : (
-                    <div className="text-sm font-semibold text-green-900">
-                      {expenditure.expenditureAmount}
+                  </div>
+                </TableData>
+                <TableData>
+                  <div className="ml-0">
+                    <div className="text-sm text-gray-900">
+                      {expenditure.expenditureDescription}
                     </div>
-                  )}
-                </div>
-              </TableData>
-            </tr>
-          ))}
+                  </div>
+                </TableData>
+                <TableData>
+                  <div className="ml-0">
+                    <div className="text-sm text-gray-900">
+                      {expenditure.expenditureType}
+                    </div>
+                  </div>
+                </TableData>
+                <TableData>
+                  <div className="ml-0">
+                    {expenditure.expenditureType === "EXPENDITURE" ? (
+                      <div className="text-sm font-semibold text-blue-900">
+                        {expenditure.expenditureAmount}
+                      </div>
+                    ) : (
+                      <div className="text-sm font-semibold text-green-900">
+                        {expenditure.expenditureAmount}
+                      </div>
+                    )}
+                  </div>
+                </TableData>
+              </tr>
+            ))}
         </tbody>
       </ListLayout>
     );
@@ -111,10 +167,11 @@ const Expenses = () => {
 
   return (
     <div
-    onClick={fetchExpenditureDetails}
-    onMouseEnter={fetchExpenditureDetails}
+      onClick={fetchExpenditureDetails}
+      onMouseEnter={fetchExpenditureDetails}
     >
-            {renderExpendituresTable()}
+      <div className="mb-5">{renderExpendituresTable()}</div>
+      <div className="mt-5">{renderBarChart}</div>
     </div>
   );
 };
